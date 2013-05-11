ba4850645de25aad21189456eecd1473f88921e3
Fernando Angarita <fangari@gmail.com>
2013-05-11 14:05:24 -0500

    Add website column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130511175741) do
+ActiveRecord::Schema.define(:version => 20130511190251) do
 
   create_table "companies", :force => true do |t|
     t.string "name"
@@ -68,10 +68,10 @@ ActiveRecord::Schema.define(:version => 20130511175741) do
     t.string   "twitter"
     t.string   "dribbble"
     t.string   "linkedin"
-    t.string   "website"
     t.string   "google"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "website"
   end
 
 end
