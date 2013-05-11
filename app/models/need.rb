cae31ef801059497a6be99aa3c1a210d3496c257
Travis Loncar <loncar.travis@gmail.com>
2013-05-11 13:16:33 -0500

    Create polymorphic relationship between Skills/Needs and Level.

--- app/models/need.rb
+++ app/models/need.rb
@@ -1,3 +1,4 @@
 class Need < ActiveRecord::Base
   attr_accessible :company_id, :description, :level_id, :resource_id
+  has_one :level, :as => :rankable
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,4 @@
 class Skill < ActiveRecord::Base
   attr_accessible :user_id, :resource_id, :level_id, :description
-
-
+  has_one :level, :as => :rankable
 end
